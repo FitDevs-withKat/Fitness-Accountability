3021c7dfc75cbb3414406d4d6c8fbc75141b2e1f
katalystkat <96984912+katalystkat@users.noreply.github.com>
2022-07-25 17:26:31 +0000

    Deploying to gh-pages from @ FitDevs-withKat/Fitness-Accountability@3e3db0d921510bdbcde3910eddef36df568017a1 🚀

---
+++ _next/static/9SH4MrLA5uMDV0L4aFjnD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5f9243f00aa6e99c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
