c50b40b906d503725dfd9b929907b8ee977cdabb
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-26 19:02:47 +0000

    fix: layout

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,9 @@
 export default function Layout({ children }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <main className="mx-auto w-full md:container">{children}</main>
+    <div className="flex min-h-screen flex-col md:mx-0">
+      <main className="mx-auto w-full px-4 md:container  md:px-0">
+        {children}
+      </main>
     </div>
   );
 }
