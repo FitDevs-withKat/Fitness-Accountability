6213530383f16ddb7b7cbcf570444ac62898ad86
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-30 08:27:56 +0000

    fix: images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-const prodSubfolder = "/Fitness-Accountability";
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? prodSubfolder : "/",
 };
 
 module.exports = nextConfig;
