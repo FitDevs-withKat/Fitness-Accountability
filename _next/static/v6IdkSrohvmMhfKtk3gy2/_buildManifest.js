bc878b03c11e453b2f8546397ad22d02a7b5c664
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-30 19:13:03 +0000

    Deploying to gh-pages from @ FitDevs-withKat/Fitness-Accountability@0a6508169a895807dea4aac4135c047f7b8b670f 🚀

--- _next/static/v6IdkSrohvmMhfKtk3gy2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ce997e9fd95ec9fc.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
