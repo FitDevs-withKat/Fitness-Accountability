8be98e49ca2ca36ace6ad4e549dcee757872177b
Lorenze Jay <lorenzejaytech@gmail.com>
2022-07-31 15:16:07 -0700

    search functionality based on profile.name and location

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,36 @@ import fs from "fs";
 import Head from "next/head";
 import Profile from "../components/Profile";
 import { ProfileType } from "../types/ProfileType";
+import SearchFilter from "../components/SearchFilter";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home({ profiles }: { profiles: ProfileType[] }) {
+  const [searchString, setSearchString] = useState("")
+  const [filteredProfiles, setFilteredProfiles] = useState<ProfileType[]>([])
+
+  const filterBySearchString = useCallback(() => {
+    if(searchString !== ""){
+      const filteredProfiles = profiles.filter(profile => profile.name.toLowerCase().includes(searchString) || profile.location.toLowerCase().includes(searchString))
+      setFilteredProfiles(filteredProfiles)
+    } else {
+      setFilteredProfiles([])
+    }
+  },[profiles, searchString])
+
+  useEffect(() => {
+    filterBySearchString()
+  },[searchString, filterBySearchString])
+
   return (
     <main className="container mx-auto">
       <Head>
         <title>Fitness-Accountability</title>
       </Head>
-      {profiles.map((profile) => (
-        <Profile key={profile.name} profile={profile} />
-      ))}
+      <SearchFilter searchString={searchString} setSearchString={setSearchString} />
+      { filteredProfiles.length === 0 && searchString === ""
+        ? profiles.map((profile) => <Profile key={profile.name} profile={profile} />) 
+        : filteredProfiles.map((profile) => <Profile key={profile.name} profile={profile} />) 
+      }
     </main>
   );
 }
