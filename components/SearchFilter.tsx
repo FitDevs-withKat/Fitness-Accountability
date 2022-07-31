8516a58e9bcec4dbed4c74c4a39d8a77faffcd84
Lorenze Jay <lorenzejaytech@gmail.com>
2022-07-31 15:57:02 -0700

    making mobile input field longer

--- components/SearchFilter.tsx
+++ components/SearchFilter.tsx
@@ -9,7 +9,7 @@ const SearchFilter = ({ searchString, setSearchString } : SearchFilterProps) => 
   return (
     <div className='py-4 flex justify-end items-end'>
       <input
-        className='rounded-md px-4 py-2 outline-none w-1/3'
+        className='rounded-md px-4 py-2 outline-none w-3/4 sm:w-1/3'
         placeholder='Search for user or by location' 
         onChange={e => setSearchString(e.target.value)} 
         value={searchString}
