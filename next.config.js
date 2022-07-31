0e4abc3d8dc1b276390da0ad3673605d8cad9096
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-07-31 12:34:56 +0000

    fix: minor

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const prodSubfolder = "/Fitness-Accountability";
-const isProd = process.env.NODE_ENV === "production";
-
-const basePath = isProd ? prodSubfolder : undefined;
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   assetPrefix: "./",
-  basePath,
 };
 
 module.exports = nextConfig;
