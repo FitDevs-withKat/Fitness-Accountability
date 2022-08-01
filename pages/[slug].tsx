2fc2458ccc749eae17c98cef32de366dcd44cf56
kantiprasanna <kantiprasanna3@gmail.com>
2022-08-01 07:24:23 +0530

    Addresses #54 title added in slug.tsx

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,18 +2,25 @@ import { loadProfilePaths, loadSingleProfile } from "../util/load-profiles";
 import ProfileDetail from "../components/profile_detail/personal-card";
 import DetailCard from "../components/profile_detail/detail-card";
 import path from "path";
+import Head from "next/head";
 import { ProfileType } from "../types/ProfileType";
 
 export default function DetailProfile({ profile }: { profile: ProfileType }) {
   return (
-    <div className="no-wrap mt-8 flex flex-col gap-4 md:flex-row">
-      <div className="w-full md:w-1/3">
-        <ProfileDetail profile={profile} />
-      </div>
-      <div className="w-full md:w-3/4">
-        <DetailCard profile={profile} />
+    <div>
+      <Head>
+        <title>Fitness-Accountability</title>
+      </Head>
+      <div className="no-wrap mt-8 flex flex-col gap-4 md:flex-row">
+        <div className="w-full md:w-1/3">
+          <ProfileDetail profile={profile} />
+        </div>
+        <div className="w-full md:w-3/4">
+          <DetailCard profile={profile} />
+        </div>
       </div>
     </div>
+    
   );
 }
 export async function getStaticPaths() {
