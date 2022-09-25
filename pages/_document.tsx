13d7f6cafbe1a4158f1f7bca013c84f72219d457
324Hz <1024Bitcoins@gmail.com>
2022-09-25 16:37:06 -0500

    Revert "commit add favicon"
    This reverts commit 22a3231e97204b13a418f26dee4caacd4c2c5172.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href={`${basePath}/fitdevslogo.ico`} />
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
       </Head>
       <body>
         <Main />
