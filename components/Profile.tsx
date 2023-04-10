7f7156f6ee87861b767a2ef14d6a8a0933408408
324Hz <1024Bitcoins@gmail.com>
2023-04-10 15:02:32 -0400

    formatted icon on profile card

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -4,43 +4,30 @@ import { basePath } from "../util/helper";
 import Image from "next/image";
 
 export default function OverviewCard({ profile }: { profile: ProfileType }) {
+  // TODO: check if there is no url provided in "profile.image" and if so, automatically fill it with the fitdevs logo
+
+  if (profile.image == "") {
+    console.log(profile.username + " does not have a profile image")
+  }
+
   return (
     <Link href={`${basePath}/${profile.username}`}>
       <div className="my-4 mx-auto flex w-full cursor-pointer flex-col rounded-lg bg-white p-4 transition-all hover:shadow-xl ">
         <div className="text-400 flex items-center text-lg font-semibold">
-          <a
-          target="_blank"
-          rel="noreferrer"
-          className=""
-          href={profile.url}>
-            <Image
-              alt="User profile picture" 
-              src={profile.image}
-              className="card-profile-image"
-              width={20}
-              height={20}
-            />
+
+          <a target="_blank" rel="noreferrer" className="" href={profile.url}>
+            <Image alt="User profile picture" src={profile.image} className="card-profile-image" width={48} height={48}/>
           </a>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="transition-all delay-75 duration-200 hover:underline"
-            href={profile.url}
-          >
+          
+          <a target="_blank" rel="noreferrer" className="pl-2 transition-all delay-75 duration-200 hover:underline" href={profile.url} >
             {profile.name}
           </a>
-          <div className="ml-4">@{profile.username}</div>
+          
+          <div className="ml-2">@{profile.username}</div>
         </div>
 
-        <div className="mt-4">
-          <Image
-            alt="SVG of Location Pointer"
-            className="location-icon"
-            width={16}
-            height={16}
-            src={`${basePath}/location.svg`}
-          />
-          {profile.location}
+        <div>
+          <Image alt="SVG of Location Pointer" width={32} height={32} src={`${basePath}/location.svg`} />{profile.location}
         </div>
         <div className="mt-4">
           My goal is:
