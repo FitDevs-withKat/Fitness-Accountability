72244b45cab5d570d66e87355680a605f887cfeb
324Hz <1024Bitcoins@gmail.com>
2023-04-09 20:04:46 -0400

    image testing commit
    testing out a layout for the profile image

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -9,6 +9,19 @@ export default function OverviewCard({ profile }: { profile: ProfileType }) {
       <div className="my-4 mx-auto flex w-full cursor-pointer flex-col rounded-lg bg-white p-4 transition-all hover:shadow-xl ">
         <div className="text-400 flex items-center text-lg font-semibold">
           <a
+          target="_blank"
+          rel="noreferrer"
+          className=""
+          href={profile.url}>
+            <Image
+              alt="User profile picture" 
+              src={profile.image}
+              className="card-profile-image"
+              width={20}
+              height={20}
+            />
+          </a>
+          <a
             target="_blank"
             rel="noreferrer"
             className="transition-all delay-75 duration-200 hover:underline"
@@ -23,8 +36,8 @@ export default function OverviewCard({ profile }: { profile: ProfileType }) {
           <Image
             alt="SVG of Location Pointer"
             className="location-icon"
-            width={32}
-            height={32}
+            width={16}
+            height={16}
             src={`${basePath}/location.svg`}
           />
           {profile.location}
--- components/overview-card.tsx
+++ components/overview-card.tsx
@@ -6,8 +6,20 @@ export default function OverviewCard({ profile }: { profile: ProfileType }) {
   return (
     <a href={`${basePath}/${profile.username}`}>
       <div className="m-4 flex w-full cursor-pointer flex-col rounded-lg border-8 border-sky-500 bg-white p-4 transition-all hover:shadow-xl">
+      <Image
+          alt="user profile picture" 
+          src="https://avatars.githubusercontent.com/u/8262590?s=80&v=4"
+          className="card-profile-image"
+          width={10}
+          height={10}
+        />
         <div className="text-400 flex items-center text-lg font-semibold">
-          <a target="_blank" rel="noreferrer" className="transition-all delay-75 duration-200 hover:underline" href={profile.url}>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="transition-all delay-75 duration-200 hover:underline"
+            href={profile.url}
+          >
             {profile.name}
           </a>
           <div className="ml-4">@{profile.username}</div>
@@ -17,8 +29,8 @@ export default function OverviewCard({ profile }: { profile: ProfileType }) {
           <Image
             className="location-icon"
             alt="SVG of Location Pointer"
-            width={32}
-            height={32}
+            width={10}
+            height={10}
             src={`${basePath}/location.svg`}
           />
           {profile.location}
